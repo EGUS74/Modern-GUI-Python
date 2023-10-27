2360003744537f88320aca742766a258309886f6
Alpha <egus74@yahoo.com>
2023-10-27 00:25:54 -0400

    App commit

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
 import customtkinter as ctk
 
-ctk.set_appearance_mode("system")
-ctk.set_default_color_theme("dark-blue")
+ctk.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
+ctk.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
-root = tk.Tk()
+root = ctk.CTk()
+root.geometry("500x350")
+              
+def login():
+    print("Login succesfully!")
 
-label = tk.Label(root, )
+
+frame = ctk.CTkFrame(master=root)
+frame.pack(pady=20, padx=60, fill="both", expand=True)
+
+
+label = ctk.CTkLabel(master=frame, text="Login System" )
+label.pack(pady=12, padx=10)
+
+entry1=ctk.CTkEntry(master=frame, placeholder_text="Username")
+entry1.pack(pady=12, padx=10)
+
+entry2=ctk.CTkEntry(master=frame, placeholder_text="Password", show="*")
+entry2.pack(pady=12, padx=10)
+
+
+button=ctk.CTkButton(master=frame, text="Login", command=login)
+button.pack(pady=12, padx=10)
+
+checkbox=ctk.CTkCheckBox(master=frame, text="Remember Me")
+checkbox.pack(pady=12, padx=10)
+
+root.mainloop()
